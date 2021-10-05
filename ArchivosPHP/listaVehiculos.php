0d6448c79545c34d56b45aaf236e6c0745e62ad2
Jonatan Gomez <jgp0007@alu.medac.es>
2021-10-05 13:18:48 +0200

    archivos actualizados

--- ArchivosPHP/listaVehiculos.php
+++ ArchivosPHP/listaVehiculos.php
@@ -1,3 +1,38 @@
+<pre>
+<?php
+
+    $usu = $_GET['id_usuario'];
+    $veh = $_GET['id_vehiculo'];
+    $db_host = "10.192.240.8:81";
+    $db_name = "taller";
+    $db_user = "admin";
+    $db_pass = "Admin123";
+    
+    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    
+    if (mysqli_connect_error()) {
+        echo mysqli_connect_error();
+        exit;
+    }
+    
+    echo "Connected successfully.";
+ 
+    $sql = "SELECT *
+            FROM lista_servicios
+            ORDER BY date_entry;";
+ 
+    $results = mysqli_query($conn, $sql);
+ 
+    if ($results === false) {
+        echo mysqli_error($conn);
+    } else {
+        $users = mysqli_fetch_all($results, MYSQLI_ASSOC);
+ 
+        print_r($users);
+    }
+ 
+?>
+</pre>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,39 +44,7 @@
 </head>
 
 <body>
-    <pre>
-<?php
 
-$db_host = "10.192.240.8:81";
-$db_name = "mycars";
-$db_user = "mycarsuser";
-$db_pass = "IF7GphtPZI_K*aMS";
-
-$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
-if (mysqli_connect_error()) {
-    echo mysqli_connect_error();
-    exit;
-}
-
-echo "Connected successfully.";
-
-$sql = "SELECT *
-            FROM user
-            ORDER BY date_entry;";
-
-$results = mysqli_query($conn, $sql);
-
-if ($results === false) {
-    echo mysqli_error($conn);
-} else {
-    $users = mysqli_fetch_all($results, MYSQLI_ASSOC);
-
-    print_r($users);
-}
-
-?>
-</pre>
 </body>
 
 </html>
