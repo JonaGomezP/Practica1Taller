14566825111166ac52f2b4d4d671f6f303281528
Jonatan Gomez <jgp0007@alu.medac.es>
2021-10-05 14:42:09 +0200

    listaVehiculos.php actualizado

--- ArchivosPHP/listaVehiculos.php
+++ ArchivosPHP/listaVehiculos.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background: black;
+            color: white;
+            font-size: 20px;
+        }
+    </style>
+</head>
+<body>
 <pre>
 <?php
 
@@ -12,9 +28,6 @@
         echo mysqli_connect_error();
         exit;
     }
-    //Usuario mete username
-    $unsername = "Rober";
-    
     
     echo "Connected successfully.";
  
@@ -30,5 +43,6 @@
  
         print_r($users);
     }
- 
 ?>
+</body>
+</html>
